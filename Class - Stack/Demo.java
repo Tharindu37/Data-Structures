a2f298725b42763dc3b7f350e077ff2395c3e2a8
Tharindu37 <tharindut520@gmail.com>
2022-09-24 23:44:23 +0530

    Created the array in the constructor

--- Class - Stack/Demo.java
+++ Class - Stack/Demo.java
@@ -1,8 +1,12 @@
 class Stack{
 	private int array[];
+	
+	Stack(int size){
+		array=new int[size];
+	}
 }
 class Demo{
 	public static void main(String args[]){
-		Stack s1=new Stack();
+		Stack s1=new Stack(10);
 	}
 }

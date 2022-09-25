4d680b703987184b1fe8fa8b7ea05dcbdf849e87
Tharindu37 <tharindut520@gmail.com>
2022-09-25 06:58:55 +0530

    Inserted values to an array

--- Class - Stack/Demo.java
+++ Class - Stack/Demo.java
@@ -1,12 +1,21 @@
 class Stack{
 	private int array[];
+	private int nextIndex = 0;
 	
 	Stack(int size){
 		array=new int[size];
+	}
+	
+	public void push(int a){
+		array[nextIndex++]=a;
 	}
 }
 class Demo{
 	public static void main(String args[]){
 		Stack s1=new Stack(10);
+		s1.push(10);
+		s1.push(20);
+		s1.push(30);
+		s1.push(40);
 	}
 }

1b82a05e96c05af1121b8009eda3548b7bdfddc4
Tharindu37 <tharindut520@gmail.com>
2022-09-27 07:03:56 +0530

    Variable initialization

--- Class - Queue/Demo.java
+++ Class - Queue/Demo.java
@@ -2,10 +2,14 @@ class Queue{
 	private int[] array;
 	private int nextIndex;
 	
+	Queue(int size){
+		array=new int[size];
+		nextIndex=0;
+	}
 }
 
 class Demo{
 	public static void main(String args[]){
-		Queue q1=new Queue();
+		Queue q1=new Queue(10);
 	}
 }

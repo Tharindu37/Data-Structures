aad76e2c3dee6ada49e39eaecf2c6278485c2e5e
Tharindu37 <tharindut520@gmail.com>
2022-09-27 07:17:04 +0530

    Inserted the value to Queue

--- Class - Queue/Demo.java
+++ Class - Queue/Demo.java
@@ -6,10 +6,19 @@ class Queue{
 		array=new int[size];
 		nextIndex=0;
 	}
+	
+	public void enQueue(int data){
+		array[nextIndex++]=data;
+	}
 }
 
 class Demo{
 	public static void main(String args[]){
 		Queue q1=new Queue(10);
+		q1.enQueue(10); //s1.push(10)
+		q1.enQueue(20);
+		q1.enQueue(30);
+		q1.enQueue(40);
+		q1.enQueue(50);
 	}
 }

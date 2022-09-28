350c78ffef5e968dd3729e39caf180149efb1fa5
Tharindu37 <tharindut520@gmail.com>
2022-09-28 07:18:23 +0530

    Print stack

--- Dynamic linked list/Stack/Demo.java
+++ Dynamic linked list/Stack/Demo.java
@@ -6,6 +6,16 @@ class Stack{
 		n1.next=top;
 		top=n1;
 	}
+	
+	public void printStack(){
+		Node temp = top;
+		System.out.print("[");
+		while(temp != null){
+			System.out.print(temp.data+", ");
+			temp=temp.next;
+		}
+		System.out.println(top==null?"empty]":"\b\b]");
+	}
 }
 class Node{
 	int data;
@@ -17,10 +27,12 @@ class Node{
 }
 class Demo{
 	public static void main(String args[]){
-		Stack s1=new Stack(); //Step 1
-		s1.push(10); //Step 2
-		s1.push(20); //Step 3
-		s1.push(30); //Step 4
-		s1.push(40); //Step 5
+		Stack s1=new Stack();
+		s1.push(10);
+		s1.push(20);
+		s1.push(30);
+		s1.push(40);
+		s1.push(50);
+		s1.printStack(); //[50, 40, 30, 20, 10]
 	}
 }

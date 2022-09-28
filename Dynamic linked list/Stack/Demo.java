768745f6c02ccf6494353ed28fd912046bedc784
Tharindu37 <tharindut520@gmail.com>
2022-09-28 07:12:21 +0530

    Inserted the data in to the Stack

--- Dynamic linked list/Stack/Demo.java
+++ Dynamic linked list/Stack/Demo.java
@@ -1,5 +1,11 @@
 class Stack{
 	private Node top;
+	
+	public void push(int data){
+		Node n1 = new Node(data);
+		n1.next=top;
+		top=n1;
+	}
 }
 class Node{
 	int data;
@@ -11,6 +17,10 @@ class Node{
 }
 class Demo{
 	public static void main(String args[]){
-		Stack s1=new Stack();
+		Stack s1=new Stack(); //Step 1
+		s1.push(10); //Step 2
+		s1.push(20); //Step 3
+		s1.push(30); //Step 4
+		s1.push(40); //Step 5
 	}
 }

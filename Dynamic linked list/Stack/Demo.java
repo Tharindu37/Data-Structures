3d4b25decfc48155e19de04aad7e4801927d96fc
Tharindu37 <tharindut520@gmail.com>
2022-09-28 06:58:18 +0530

    Declarated the constructor in the node class

--- Dynamic linked list/Stack/Demo.java
+++ Dynamic linked list/Stack/Demo.java
@@ -1,9 +1,13 @@
 class Node{
 	int data;
 	Node next;
+	
+	Node(int data){
+		this.data=data;
+	}
 }
 class Demo{
 	public static void main(String args[]){
-		Node n1=new Node();
+		Node n1=new Node(10);
 	}
 }

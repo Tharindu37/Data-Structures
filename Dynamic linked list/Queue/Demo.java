6ac0b50c4aa4aa833a549a4c36494776ff70a16f
Tharindu37 <tharindut520@gmail.com>
2022-09-29 05:17:12 +0530

    Create Queue class

---
+++ Dynamic linked list/Queue/Demo.java
@@ -0,0 +1,15 @@
+class Queue{
+	//-----"Node" inner class-------
+	class Node{
+		private int data;
+		private Node next;
+		private Node(int data){
+			this.data=data;
+		}
+	}
+}
+class Demo{
+	public static void main(String args[]){
+		Queue q1=new Queue();
+	}
+}

53ced55eda097c0b0cbfda517a0de173ab3aa5b5
Tharindu37 <tharindut520@gmail.com>
2022-09-28 07:33:34 +0530

    Created isEmpty and size method

--- Dynamic linked list/Stack/Demo.java
+++ Dynamic linked list/Stack/Demo.java
@@ -17,6 +17,20 @@ class Stack{
 		System.out.println(top==null?"empty]":"\b\b]");
 	}
 	
+	public boolean isEmpty(){
+		return top==null;
+	}
+	
+	public int size(){
+		int count=0;
+		Node temp=top;
+		while(temp!=null){
+			count++;
+			temp=temp.next;
+		}
+		return count;
+	}
+	
 	//------"Node" Inner class--------
 	class Node{
 		private int data;
@@ -31,11 +45,15 @@ class Stack{
 class Demo{
 	public static void main(String args[]){
 		Stack s1=new Stack();
+		System.out.println("Size of the stack : "+s1.size()); //0
+		System.out.println("Stack is empty..: "+s1.isEmpty()); //true
 		s1.push(10);
 		s1.push(20);
 		s1.push(30);
 		s1.push(40);
 		s1.push(50);
 		s1.printStack(); //[50, 40, 30, 20, 10]
+		System.out.println("Size of the stack : "+s1.size()); //5
+		System.out.println("Stack is empty..: "+s1.isEmpty()); //false
 	}
 }

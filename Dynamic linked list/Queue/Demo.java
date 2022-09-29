b2b858c0398a6c7a14a00319f8620bcd4df4290f
Tharindu37 <tharindut520@gmail.com>
2022-09-29 05:40:21 +0530

    Insert data in to Queue

--- Dynamic linked list/Queue/Demo.java
+++ Dynamic linked list/Queue/Demo.java
@@ -1,6 +1,20 @@
 class Queue{
 	private Node front;
 	
+	
+	public void enQueue(int data){
+		Node n1=new Node(data);
+		if(front==null){
+			front=n1;
+		}else{
+			Node lastNode=front;
+			while(lastNode.next!=null){
+				lastNode=lastNode.next;
+			}
+			lastNode.next=n1;
+		}
+	}
+	
 	//-----"Node" inner class-------
 	class Node{
 		private int data;
@@ -13,5 +27,9 @@ class Queue{
 class Demo{
 	public static void main(String args[]){
 		Queue q1=new Queue();
+		q1.enQueue(10);
+		q1.enQueue(20);
+		q1.enQueue(30);
+		q1.enQueue(40);
 	}
 }
